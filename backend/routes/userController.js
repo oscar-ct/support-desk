f91918d5ee20b37a3d3410cc33b5460d05f5a12c
Oscar Castro <oscar.a.castro818@gmail.com>
2023-04-15 12:25:01 -0500

    feat: successfully created user, hashed password w bcrypt

--- backend/routes/userController.js
+++ backend/routes/userController.js
@@ -1,5 +1,7 @@
 
 const asyncHandler = require("express-async-handler");
+const bcrypt = require("bcryptjs");
+const User = require("../models/userModel");
 
 
 // These functions are used to clean up routes @ userFiles
@@ -9,11 +11,34 @@ const asyncHandler = require("express-async-handler");
 const registerUser = asyncHandler(async (req, res) => {
     const {name, email, password} = req.body;
     // validation
-    if(!name || email || password) {
+    if(!name || !email || !password) {
         res.status(400);
         throw new Error("Please include all fields")
+    } else {
+        const userExists = await User.findOne({email: email});
+        if (userExists) {
+            res.status(400);
+            throw new Error("User already exists");
+        } else {
+            const salt = await bcrypt.genSalt(10);
+            const hashedPassword = await bcrypt.hash(password, salt);
+            const user = await User.create({
+                name,
+                email,
+                password: hashedPassword,
+            });
+            if(user) {
+                res.status(201).json({
+                    _id: user._id,
+                    name: user.name,
+                    email: user.email,
+                });
+            } else {
+                res.status(400);
+                throw new Error("Invalid user data")
+            }
+        }
     }
-    res.send("Register Route")
 });
 
 
