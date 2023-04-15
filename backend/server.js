9484686e2fd9c63ea83b2c853acae607d4e3c9b3
Oscar Castro <oscar.a.castro818@gmail.com>
2023-04-14 22:19:59 -0500

    setup server js, tested port, tested good.

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,20 @@
-console.log("Server....")
+const express = require('express');
+const dotenv = require('dotenv').config();
+
+const PORT = process.env.PORT || 6000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.status(200).json({message: "Welcome to the Support Desk API"});
+});
+
+
+app.use("/api/users", require("./routes/userFiles"))
+
+app.listen(6000, 'localhost', () => {
+    console.log('working');
+    console.log(`Server started on port ${PORT}`);
+});
+
+
