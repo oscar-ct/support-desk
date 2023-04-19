22add17b7c0425160bbd95ff5c7f043cd6dba8b6
Oscar Castro <oscar.a.castro818@gmail.com>
2023-04-18 22:00:51 -0500

    feat: add new reducer

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,8 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+// import counterReducer from '../features/counter/counterSlice';
+import authReducer from "../features/auth/authSlice"
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    // counter: counterReducer,
+    auth: authReducer,
+
   },
 });
