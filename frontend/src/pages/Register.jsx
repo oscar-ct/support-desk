42f8c6957917da2e17e3845b34b0ea5e2337a782
Oscar Castro <oscar.a.castro818@gmail.com>
2023-04-18 20:06:44 -0500

    feat: created register form, local state, and toast

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,10 +1,95 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {FaUser} from "react-icons/fa";
+import {toast} from "react-toastify";
 
 const Register = () => {
+    const [formData, setFormData] = useState({
+        name: "",
+        email: "",
+        password: "",
+        password2: "",
+    });
+
+    const { name, email, password, password2 } = formData;
+
+    const onChange = (e) => {
+        setFormData(prevState => {
+            return {
+                ...prevState,
+                [e.target.name]: e.target.value
+            }
+        });
+    }
+
+    const registerUser = (e) => {
+        e.preventDefault();
+        if (password2 !== password) {
+            toast.error("Passwords do not match")
+        } else {
+
+        }
+    }
+
     return (
-        <div>
-            Register
-        </div>
+        <>
+            <section>
+                <h1>
+                    <FaUser/>Register
+                </h1>
+                <p>Please create an account</p>
+            </section>
+            <section>
+                <form onSubmit={registerUser}>
+                    <div>
+                        <input
+                        type={"text"}
+                        value={name}
+                        name={"name"}
+                        onChange={onChange}
+                        placeholder={"John Doe"}
+                        required
+                        />
+                    </div>
+                    <div>
+                        <input
+                            type={"email"}
+                            value={email}
+                            name={"email"}
+                            onChange={onChange}
+                            placeholder={"email@gmail.com"}
+                            required
+                        />
+                    </div>
+                    <div>
+                        <input
+                            type={"password"}
+                            value={password}
+                            name={"password"}
+                            onChange={onChange}
+                            placeholder={"Enter password"}
+                            required
+                        />
+                    </div>
+                    <div>
+                        <input
+                            type={"password2"}
+                            value={password2}
+                            name={"password2"}
+                            onChange={onChange}
+                            placeholder={"Confirm password"}
+                            required
+                        />
+                    </div>
+                    <div>
+                        <button
+                        type={"submit"}
+                        >
+                            Create
+                        </button>
+                    </div>
+                </form>
+            </section>
+        </>
     );
 };
 
