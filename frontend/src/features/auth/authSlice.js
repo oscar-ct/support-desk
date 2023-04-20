4111e8182db4fc7e43592957245d40335e8d44bd
Oscar Castro <oscar.a.castro818@gmail.com>
2023-04-19 23:53:18 -0500

    feat: try catch post request to backend, setup reset state reducer

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -1,4 +1,5 @@
 import {createSlice, createAsyncThunk} from "@reduxjs/toolkit";
+import authService from "./authService";
 
 const initialState = {
     user: null,
@@ -11,6 +12,15 @@ const initialState = {
 
 export const registerUser = createAsyncThunk("auth/register", async (user, thunkAPI) => {
     console.log(user);
+    try {
+        return await authService.commitRegister(user);
+    } catch (e) {
+        console.log(e);
+        console.log(e.response.error);
+        console.log(e.response);
+        console.log(e.response.data);
+        return thunkAPI.rejectWithValue(e.response.data.toString());
+    }
 });
 
 export const loginUser = createAsyncThunk("auth/login", async (user, thunkAPI) => {
@@ -21,10 +31,19 @@ export const loginUser = createAsyncThunk("auth/login", async (user, thunkAPI) =
 export const authSlice = createSlice({
     name: "auth",
     initialState,
-    reducers: {},
+    reducers: {
+        reset: (state) => {
+            state.isLoading = false;
+            state.isError = false;
+            state.isSuccess = false;
+            state.message = "";
+        }
+
+    },
     extraReducers: (builder => {
-        // todo
+
     }),
 });
 
+export const {reset} = authSlice.actions;
 export default authSlice.reducer;
