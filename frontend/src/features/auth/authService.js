3c1a4cb829431a9ee3da35433ba96a40599c889f
Oscar Castro <oscar.a.castro818@gmail.com>
2023-04-19 23:50:03 -0500

    feat: created post request for backend, testing this function

---
+++ frontend/src/features/auth/authService.js
@@ -0,0 +1,28 @@
+
+const API_URL = "/api/users"
+
+const commitRegister = async (userData) => {
+    const options = {
+        method: "POST",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json;charset=UTF-8",
+        },
+        body: JSON.stringify(userData),
+    };
+    const response = await fetch(API_URL, options);
+    const data = response.json();
+    console.log(data);
+    // if (data) {
+    //     localStorage.setItem("user", JSON.stringify(data));
+    // }
+    return data;
+}
+
+const authService = {
+    commitRegister
+}
+
+export default authService;
+
+
