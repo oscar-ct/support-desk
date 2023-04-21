9cf49f461f831f09d3ee7d97394b9d18487b66ec
Oscar Castro <oscar.a.castro818@gmail.com>
2023-04-21 18:31:18 -0500

    feat: create new func for logout btn, calls thunk and reset reducer, set user conditional for logout, login, and register

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,25 +1,49 @@
 import React from 'react';
 import {FaSignInAlt, FaSignOutAlt, FaUser} from "react-icons/fa";
-import {Link} from "react-router-dom";
-
+import {Link, useNavigate} from "react-router-dom";
+import {useDispatch, useSelector} from "react-redux";
+import {logoutFunc, resetFunc} from "../features/auth/authSlice";
 
 const Header = () => {
+
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const {user} = useSelector(state => state.auth);
+    
+    const onClickLogout = () => {
+        dispatch(logoutFunc());
+        dispatch(resetFunc());
+        navigate('/');
+    }
+
+
     return (
         <header className={"header"}>
             <div className={"logo"}>
                 <Link to={"/"}>Support Desk</Link>
             </div>
                 <ul>
-                    <li>
-                        <Link to={"/login"}>
-                        <FaSignInAlt/> Login
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to={"/register"}>
-                            <FaUser/> Register
-                        </Link>
-                    </li>
+                    {user ? (
+                        <li>
+                            <button className={"btn"} type={"button"} onClick={onClickLogout}>
+                                <FaSignOutAlt/>Logout
+                            </button>
+                        </li>
+                    ) : (
+                        <>
+                            <li>
+                                <Link to={"/login"}>
+                                    <FaSignInAlt/> Login
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to={"/register"}>
+                                    <FaUser/> Register
+                                </Link>
+                            </li>
+                        </>
+                    )}
+
                 </ul>
 
         </header>
