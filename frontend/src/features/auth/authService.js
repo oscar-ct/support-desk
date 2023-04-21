553c19480107b589842009d0741773a782e877f0
Oscar Castro <oscar.a.castro818@gmail.com>
2023-04-21 01:28:45 -0500

    feat: axios fixes issue with redux not recognizing rejection, set userdata to local storage to store active session

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -2,34 +2,31 @@ import axios from "axios";
 
 const API_URL = "/api/users"
 
+const commitRegister = async (userData) => {
+    const response = await axios.post(API_URL, userData);
+    if (response.data) {
+        localStorage.setItem("user", JSON.stringify(response.data));
+    }
+    return response.data;
+}
+
+
 // const commitRegister = async (userData) => {
-//     const response = await axios.post(API_URL, userData);
-//     const data = response.json();
-//     console.log(data);
+//     const options = {
+//         method: "POST",
+//         headers: {
+//             Accept: "application/json",
+//             "Content-Type": "application/json;charset=UTF-8",
+//         },
+//         body: JSON.stringify(userData),
+//     };
+//     const response = await fetch(API_URL, options);
 //     // if (data) {
 //     //     localStorage.setItem("user", JSON.stringify(data));
 //     // }
+//     const data = await response.json()
 //     return data;
 // }
-
-
-const commitRegister = async (userData) => {
-    const options = {
-        method: "POST",
-        headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json;charset=UTF-8",
-        },
-        body: JSON.stringify(userData),
-    };
-    const response = await fetch(API_URL, options);
-    const data = response.json();
-    // console.log(data);
-    // if (data) {
-    //     localStorage.setItem("user", JSON.stringify(data));
-    // }
-    return data;
-}
 
 const authService = {
     commitRegister
