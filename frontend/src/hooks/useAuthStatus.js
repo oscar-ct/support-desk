f154245e732a3345069ecdfc00dddc6f8786d239
Oscar Castro <oscar.a.castro818@gmail.com>
2023-04-24 23:52:28 -0500

    feat: created new custom hook to verify the user is logged in

---
+++ frontend/src/hooks/useAuthStatus.js
@@ -0,0 +1,24 @@
+import {useEffect, useState,} from "react";
+import {useSelector} from "react-redux";
+
+export const useAuthStatus = () => {
+
+    const [loggedIn, setLoggedIn] = useState(false);
+    const [checkingStatus, setCheckingStatus] = useState(true);
+
+    const {user} = useSelector((state) => {
+        return state.auth;
+    });
+
+    useEffect(function () {
+        if (user) {
+            setLoggedIn(true);
+        } else {
+            setLoggedIn(false);
+        }
+        setCheckingStatus(false);
+    }, []);
+
+    return { loggedIn, checkingStatus }
+
+}
