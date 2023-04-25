a9e42fdbfc69c827284cd4e12acb53ca5eaea79d
Oscar Castro <oscar.a.castro818@gmail.com>
2023-04-24 23:55:29 -0500

    feat: created a private route component that verifies a users auth status and redirects to the set page

---
+++ frontend/src/components/PrivateRoute.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Navigate, Outlet} from "react-router-dom";
+import { useAuthStatus } from "../hooks/useAuthStatus"
+
+
+const PrivateRoute = () => {
+
+    const { loggedIn, checkingStatus } = useAuthStatus();
+
+    if (checkingStatus) {
+        return <h1>Loading</h1>
+    }
+
+    if (loggedIn) {
+        return <Outlet/>
+    } else {
+        return <Navigate to={"/login"}/>
+    }
+
+};
+
+export default PrivateRoute;
