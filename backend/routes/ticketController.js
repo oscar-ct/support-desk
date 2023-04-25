8382f5c66cb52d22c5468edae34835b5479e0fd7
Oscar Castro <oscar.a.castro818@gmail.com>
2023-04-24 23:23:41 -0500

    setup CRUD ticket actions

--- backend/routes/ticketController.js
+++ backend/routes/ticketController.js
@@ -16,13 +16,121 @@ const getTickets = asyncHandler( async(req, res) => {
     if (user) {
         const tickets = await Ticket.find({user: req.user.id});
         res.status(201).json(tickets);
-    } else if (!user || tickets.length === 0){
+    } else if (!user){
         res.status(401).json({
             message: userErr.message,
             stack: userErr.stack,
         });
     }
 });
+
+
+
+// @route /api/tickets:id
+// @access true (Private)
+// GET
+const getTicket = asyncHandler( async(req, res) => {
+    const user = await User.findById(req.user.id);
+    if (user) {
+        const ticket = await Ticket.findById(req.params.id);
+        if (ticket?.user.toString() !== req.user.id) {
+            let err = new Error("Not authorized");
+            res.status(401).json({
+                message: err.message,
+                stack: err.stack,
+            });
+        } else {
+            res.status(201).json(ticket);
+        }
+        if (!ticket) {
+            let err = new Error("Could not find ticket");
+            res.status(401).json({
+                message: err.message,
+                stack: err.stack,
+            });
+        }
+        res.status(201).json(tickets);
+    } else if (!user){
+        res.status(401).json({
+            message: userErr.message,
+            stack: userErr.stack,
+        });
+    }
+});
+
+
+
+
+// @route /api/tickets/:id
+// @access true (Private)
+// DELETE
+const deleteTicket = asyncHandler( async(req, res) => {
+    const user = await User.findById(req.user.id);
+    if (user) {
+        const ticket = await Ticket.findById(req.params.id);
+        if (ticket?.user.toString() !== req.user.id) {
+            let err = new Error("Not authorized");
+            res.status(401).json({
+                message: err.message,
+                stack: err.stack,
+            });
+        } else {
+            await Ticket.findByIdAndDelete(req.params.id);
+            res.status(201).json({message: "Ticket has been successfully deleted"});
+        }
+        if (!ticket) {
+            let err = new Error("Could not find ticket");
+            res.status(401).json({
+                message: err.message,
+                stack: err.stack,
+            });
+        }
+        res.status(201).json(tickets);
+    } else if (!user){
+        res.status(401).json({
+            message: userErr.message,
+            stack: userErr.stack,
+        });
+    }
+});
+
+
+
+
+
+// @route /api/tickets/:id
+// @access true (Private)
+// PUT
+const updateTicket = asyncHandler( async(req, res) => {
+    const user = await User.findById(req.user.id);
+    if (user) {
+        const ticket = await Ticket.findById(req.params.id);
+        if (ticket?.user.toString() !== req.user.id) {
+            let err = new Error("Not authorized");
+            res.status(401).json({
+                message: err.message,
+                stack: err.stack,
+            });
+        } else {
+            const updatedTicket = await Ticket.findByIdAndUpdate(req.params.id, req.body, { new: true })
+            res.status(201).json(updatedTicket);
+        }
+        if (!ticket) {
+            let err = new Error("Could not find ticket");
+            res.status(401).json({
+                message: err.message,
+                stack: err.stack,
+            });
+        }
+        res.status(201).json(tickets);
+    } else if (!user){
+        res.status(401).json({
+            message: userErr.message,
+            stack: userErr.stack,
+        });
+    }
+});
+
 
 
 
@@ -63,4 +171,7 @@ const createTicket = asyncHandler( async(req, res) => {
 module.exports = {
     getTickets,
     createTicket,
+    getTicket,
+    deleteTicket,
+    updateTicket,
 }
