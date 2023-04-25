54560d4482e4308926aae036b5f019e0c3847a03
Oscar Castro <oscar.a.castro818@gmail.com>
2023-04-25 16:04:54 -0500

    feat: setup redux app store reducer

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 // import counterReducer from '../features/counter/counterSlice';
 import authReducer from "../features/auth/authSlice"
+import ticketReducer from "../features/tickets/ticketSlice";
 
 export const store = configureStore({
   reducer: {
     // counter: counterReducer,
     auth: authReducer,
+    ticket: ticketReducer
 
   },
 });
