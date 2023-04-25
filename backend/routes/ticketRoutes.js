a34966de2d449679b88029d1d2be0e7fae272b60
Oscar Castro <oscar.a.castro818@gmail.com>
2023-04-24 23:22:52 -0500

    feat: created new routes for delete and update ticket api

--- backend/routes/ticketRoutes.js
+++ backend/routes/ticketRoutes.js
@@ -1,15 +1,16 @@
 
 const express = require("express");
 const router = express.Router();
-const {getTickets, createTicket} = require("../routes/ticketController");
+const {getTickets, createTicket, getTicket} = require("../routes/ticketController");
 
 
 const {protect} = require("../middleware/authMiddleware");
+const {deleteTicket, updateTicket} = require("./ticketController");
 
 
 
 router.route("/").get(protect, getTickets).post(protect, createTicket);
 
-
+router.route("/:id").get(protect, getTicket).delete(protect, deleteTicket).put(protect, updateTicket);
 
 module.exports = router;
