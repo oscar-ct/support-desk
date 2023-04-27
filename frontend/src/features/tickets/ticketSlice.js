61838e970e18c6814b6b5e037d8f896afa26082a
Oscar Castro <oscar.a.castro818@gmail.com>
2023-04-26 20:05:05 -0500

    feat: used redux to add tickets to state

--- frontend/src/features/tickets/ticketSlice.js
+++ frontend/src/features/tickets/ticketSlice.js
@@ -24,6 +24,16 @@ export const createTicket = createAsyncThunk("ticket/create", async (ticketData,
 
 
 
+export const getUserTickets = createAsyncThunk("ticket/getAll", async (_, thunkAPI) => {
+    try {
+        const token = thunkAPI.getState().auth.user.token;
+        return await ticketService.commitGetTickets(token);
+    } catch (e) {
+        const message = e.response.data.message.toString();
+        return thunkAPI.rejectWithValue(message);
+    }
+});
+
 
 export const ticketSlice = createSlice({
     name: "ticket",
@@ -41,11 +51,24 @@ export const ticketSlice = createSlice({
             .addCase(createTicket.pending, (state) => {
                 state.isLoading = true;
             })
+            .addCase(getUserTickets.pending, (state) => {
+                state.isLoading = true;
+            })
             .addCase(createTicket.fulfilled, (state) => {
                 state.isLoading = false;
                 state.isSuccess = true;
             })
+            .addCase(getUserTickets.fulfilled, (state, action) => {
+                state.isLoading = false;
+                state.isSuccess = true;
+                state.tickets = action.payload;
+            })
             .addCase(createTicket.rejected, (state, action) => {
+                state.isLoading = false;
+                state.isError = true;
+                state.message = action.payload;
+            })
+            .addCase(getUserTickets.rejected, (state, action) => {
                 state.isLoading = false;
                 state.isError = true;
                 state.message = action.payload;
