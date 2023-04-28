e421510f7e7bee0a2152fca5c006e28cecf2f66c
Oscar Castro <oscar.a.castro818@gmail.com>
2023-04-27 19:52:31 -0500

    feat: style updates

--- frontend/src/pages/Ticket.jsx
+++ frontend/src/pages/Ticket.jsx
@@ -4,12 +4,18 @@ import {useSelector, useDispatch} from "react-redux";
 import {getUserTicket, resetFunc, updateUserTicket} from "../features/tickets/ticketSlice";
 import BackButton from "../components/BackButton";
 import {toast} from "react-toastify";
+import {getTicketNotes, resetFunc as notesResetFunc} from "../features/notes/noteSlice";
+import NoteItem from "../components/NoteItem";
 
 const Ticket = () => {
 
     const {ticket, isLoading, isSuccess, isError, message} = useSelector((state) => {
         return state.tickets;
     });
+
+    const {notes, isLoading: notesIsLoading, isSuccess: notesIsSuccess, isError: notesIsError, message: notesMessage} = useSelector((state) => {
+        return state.notes;
+    })
 
     const dispatch = useDispatch();
     const {ticketId} = useParams();
@@ -22,7 +28,9 @@ const Ticket = () => {
         }
 
         dispatch(getUserTicket(ticketId));
-        resetFunc();
+        dispatch(getTicketNotes(ticketId));
+        // resetFunc();
+        // notesResetFunc();
 
     }, [isSuccess, isError, dispatch, ticketId, message]);
 
@@ -42,7 +50,7 @@ const Ticket = () => {
         navigate("/tickets");
     }
 
-    if (isLoading) {
+    if (isLoading || notesIsLoading) {
         return <h1>Loading...</h1>
     }
 
@@ -68,6 +76,9 @@ const Ticket = () => {
                     <div className={"fw-bold fs-5"}>Product: {ticket.product}</div>
                 </div>
                 <div className={"border-bottom mb-3 mt-2"}/>
+            </section>
+
+            <section>
                 <div className={"p-3 w-100 bg-light d-flex flex-column align-items-start"}>
                     <div className={"fw-bold fs-6 mb-2"}>
                         Description of issue
@@ -76,17 +87,33 @@ const Ticket = () => {
                         {ticket.description}
                     </div>
                 </div>
-                <div className={"mt-3"}>
+            </section>
+
+            <section>
+                <div className={"mt-3 w-100 d-flex flex-column align-items-start"}>
+                    <div className={"fw-bold fs-5 mb-2"}>
+                        Notes
+                    </div>
+
+                        {
+                            notes.map(function (note) {
+                                return <NoteItem key={note._id} note={note}/>
+                            })
+                        }
+
+                </div>
+            </section>
+
+            <div className={"mt-4"}>
                 {
                     ticket.status !== "closed" && (
-                        <button onClick={closeTicket} className={"btn btn-danger"}>
+                        <button onClick={closeTicket} className={"btn btn-danger w-50"}>
                             Close Ticket
                         </button>
                     )
 
                 }
-                </div>
-            </section>
+            </div>
         </>
     );
 };
