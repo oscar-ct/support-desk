be4daff9e788dadca39f67016fd56e8fed4c412a
Oscar Castro <oscar.a.castro818@gmail.com>
2023-04-27 19:54:26 -0500

    feat: created note item component, displayed data form api and styled

---
+++ frontend/src/components/NoteItem.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {useSelector} from "react-redux";
+
+const NoteItem = ({note}) => {
+
+    const {user} = useSelector((state) => {
+        return state.auth;
+    });
+
+    return (
+        <div className={"my-2 p-3 w-100 border d-flex flex-column align-items-start"}>
+            <div className={"w-100 d-flex flex-row justify-content-between"}>
+                <div className={"fw-bold"}>
+                    Note from {note.isStaff ? <span>Staff</span> : <span>{user.name}</span>}
+                </div>
+                <div>
+                    {new Date(note.createdAt).toLocaleString("en-US")}
+                </div>
+            </div>
+            {note.note}
+        </div>
+    );
+};
+
+export default NoteItem;
