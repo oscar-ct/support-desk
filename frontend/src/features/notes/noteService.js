d9696a6c2acb5fd430ca577772ce1a96ea913062
Oscar Castro <oscar.a.castro818@gmail.com>
2023-04-27 21:41:11 -0500

    feat: reformat func params, fixed issue causing notes not to post

--- frontend/src/features/notes/noteService.js
+++ frontend/src/features/notes/noteService.js
@@ -1,18 +1,30 @@
 import axios from "axios";
 const API_URL = "/api/tickets/";
 
-const commitGetTicketNotes = async (ticketID, token) => {
+const commitGetTicketNotes = async (ticketId, token) => {
+
     const config = {
         headers: {
             Authorization: `Bearer ${token}`
         }
     }
-    const response = await axios.get(API_URL + ticketID + "/notes", config);
+    const response = await axios.get(API_URL + ticketId + "/notes", config);
+    return response.data;
+}
+
+const commitCreateTicketNote = async (noteText, ticketId, token) => {
+    const config = {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    }
+    const response = await axios.post(API_URL + ticketId + "/notes", {note: noteText}, config);
     return response.data;
 }
 
 const noteService = {
     commitGetTicketNotes,
+    commitCreateTicketNote
 
 }
 
--- frontend/src/features/notes/noteSlice.js
+++ frontend/src/features/notes/noteSlice.js
@@ -22,6 +22,17 @@ export const getTicketNotes = createAsyncThunk("notes/getAll", async (ticketId, 
 });
 
 
+export const createTicketNote = createAsyncThunk("notes/create", async ({noteText, ticketId}, thunkAPI) => {
+    try {
+        const token = thunkAPI.getState().auth.user.token;
+        return await noteService.commitCreateTicketNote(noteText, ticketId, token);
+    } catch (e) {
+        const message = e.response.data.message.toString();
+        return thunkAPI.rejectWithValue(message);
+    }
+});
+
+
 
 export const noteSlice = createSlice({
     name: "note",
@@ -42,6 +53,19 @@ export const noteSlice = createSlice({
                 state.notes = action.payload;
             })
             .addCase(getTicketNotes.rejected, (state, action) => {
+                state.isLoading = false;
+                state.isError = true;
+                state.message = action.payload;
+            })
+            .addCase(createTicketNote.pending, (state) => {
+                state.isLoading = true;
+            })
+            .addCase(createTicketNote.fulfilled, (state, action) => {
+                state.isLoading = false;
+                state.isSuccess = true;
+                state.notes.push(action.payload)
+            })
+            .addCase(createTicketNote.rejected, (state, action) => {
                 state.isLoading = false;
                 state.isError = true;
                 state.message = action.payload;
