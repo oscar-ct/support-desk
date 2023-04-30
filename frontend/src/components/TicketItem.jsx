5ae7e97c46b51f1897c1116a8a5a29211e62f74b
Oscar Castro <oscar.a.castro818@gmail.com>
2023-04-30 15:03:58 -0500

    feat: temp removed column, added click event to row, navigates to ticket page

--- frontend/src/components/TicketItem.jsx
+++ frontend/src/components/TicketItem.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import {Link} from "react-router-dom";
+import {useNavigate} from "react-router-dom";
+
+
 
 
 const ticketStatusColor = (status) => {
@@ -14,28 +16,41 @@ const ticketStatusColor = (status) => {
 
 
 const TicketItem = ({ ticket }) => {
+
+    const navigate = useNavigate();
+
+
     return (
 
-        <tbody className={"bg-light"}>
+        <tbody style={{cursor: "pointer"}} className={"cursor bg-light"}>
             <tr>
-                <td className={"py-5"}>
+                <td onClick={() => navigate("/ticket/" + ticket._id)} className={"py-4"}>
                     {new Date(ticket.createdAt).toLocaleString("en-US")}
                 </td>
-                <td className={"py-5"}>
+                <td onClick={() => navigate("/ticket/" + ticket._id)} className={"py-4"}>
                     {ticket.product}
                 </td>
-                <td className={"py-5"}>
+                <td onClick={() => navigate("/ticket/" + ticket._id)} className={"py-4"}>
                     <div className="d-flex align-items-center justify-content-center">
                         <div className={`${ticketStatusColor(ticket.status)} rounded-pill text py-1 w-100`}>
                             {ticket.status}
                         </div>
                     </div>
                 </td>
-                <td className={"py-5"}>
+                <td className={"py-4"}>
                     <div className={"d-flex justify-content-center"}>
-                        <Link className={"custom-link border border-secondary bg-white py-1 px-2 px-md-5 rounded"} to={`/ticket/${ticket._id}`}>
-                            View
-                        </Link>
+                        {/*<Link className={"custom-link border border-secondary bg-white py-1 px-2 px-md-5 rounded"} to={`/ticket/${ticket._id}`}>*/}
+                        {/*    View*/}
+                        {/*</Link>*/}
+
+                        {/*{*/}
+                        {/*    ticket.status !== "closed" && (*/}
+                        {/*        <Link to={`/edit-ticket/${ticket._id}`} className={"px-3"}>*/}
+                        {/*            <FaEdit className={"fs-5"}/>*/}
+                        {/*        </Link>*/}
+                        {/*    )*/}
+                        {/*}*/}
+
                     </div>
                 </td>
             </tr>
