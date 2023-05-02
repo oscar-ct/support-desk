6e871ca3909479ff8e5c0f9192e8212b534bb7ee
Oscar Castro <oscar.a.castro818@gmail.com>
2023-05-01 22:01:45 -0500

    feat: prepared for production

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const colors = require("colors");
 const dotenv = require('dotenv').config();
 const { errorHandler } = require("./middleware/errorMiddleware");
 const { connectDB } = require("./config/db");
+const path = require("path");
 
 
 
@@ -27,15 +28,30 @@ app.use("/api/users", require("./routes/userRoutes"));
 app.use("/api/tickets", require("./routes/ticketRoutes"));
 
 
+/// Serve Frontend
+if (process.env.NODE_ENV === "production") {
+    /// set build folder as static
+    app.use(express.static(path.join(__dirname, "../frontend/build")));
+    /// loading html page from build folder
+    app.get("*", (req, res) => res.sendFile(__dirname, "../", "frontend", "build", "index.html"));
+} else {
+    app.get('/', (req, res) => {
+        res.status(200).json({message: "Welcome to the Support Desk API"});
+    });
 
-app.get('/', (req, res) => {
-    res.status(200).json({message: "Welcome to the Support Desk API"});
-});
+}
 
 
-app.listen(5000, 'localhost', () => {
+//// add to package json
+// "heroku-postbuild" : "NPM_CONFIG_PRODUCTION=false npm install --prefix frontend && npm run build --prefix frontend"
+
+
+
+
+
+app.listen(PORT, 'localhost', () => {
     console.log('working');
-    console.log(`Server started on port 5000`);
+    console.log(`Server started on port ${PORT}`);
 });
 
 
